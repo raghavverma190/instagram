f80eca03d726322efebd7a0973fe9c9001dd311f
Raghav Verma <raghav.verma190@gmail.com>
2021-02-24 03:11:54 +0530

    added redux

---
+++ components/auth/Login.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import { View, Button, TextInput } from 'react-native';
+
+import firebase from 'firebase';
+
+export class Login extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: '',
+      password: '',
+    };
+
+    this.onSignUp = this.onSignIn.bind(this);
+  }
+
+  onSignIn() {
+    const { email, password } = this.state;
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(email, password)
+      .then((result) => {
+        firebase
+          .firestore()
+          .collection('users')
+          .doc(firebase.auth().currentUser.uid)
+          .set({
+            email,
+          });
+        console.log(result);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  render() {
+    return (
+      <View>
+        <TextInput
+          placeholder='email'
+          onChangeText={(email) => this.setState({ email })}
+        />
+        <TextInput
+          placeholder='password'
+          secureTextEntry={true}
+          onChangeText={(password) => this.setState({ password })}
+        />
+        <Button onPress={() => this.onSignIn()} title='Sign Up' />
+      </View>
+    );
+  }
+}
+
+export default Login;
