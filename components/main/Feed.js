f10c37cb2dc412d70e60e50f7f428dadc99684c1
Raghav Verma <raghav.verma190@gmail.com>
2021-05-11 18:18:24 +0530

    like/dislike implemented

--- components/main/Feed.js
+++ components/main/Feed.js
@@ -22,6 +22,29 @@ function Feed(props) {
     // console.log(posts);
   }, [props.usersFollowingLoaded, props.feed]);
 
+  const onLikePress = (userId, postId) => {
+    firebase
+      .firestore()
+      .collection('posts')
+      .doc(userId)
+      .collection('userPosts')
+      .doc(postId)
+      .collection('likes')
+      .doc(firebase.auth().currentUser.uid)
+      .set({});
+  };
+  const onDislikePress = (userId, postId) => {
+    firebase
+      .firestore()
+      .collection('posts')
+      .doc(userId)
+      .collection('userPosts')
+      .doc(postId)
+      .collection('likes')
+      .doc(firebase.auth().currentUser.uid)
+      .delete();
+  };
+
   return (
     <View style={styles.container}>
       <View style={styles.containerGallery}>
@@ -33,6 +56,17 @@ function Feed(props) {
             <View style={styles.containerImage}>
               <Text style={styles.container}>{item.user.name}</Text>
               <Image source={{ uri: item.downloadURL }} style={styles.image} />
+              {item.currentUserLike ? (
+                <Button
+                  title='Dislike'
+                  onPress={() => onDislikePress(item.user.uid, item.id)}
+                />
+              ) : (
+                <Button
+                  title='Like'
+                  onPress={() => onLikePress(item.user.uid, item.id)}
+                />
+              )}
               <Text
                 onPress={() =>
                   props.navigation.navigate('Comment', {
